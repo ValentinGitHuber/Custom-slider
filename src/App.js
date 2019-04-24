af37630f7b7351aca869586b1db8ee927c46b5de
Valentin <perlogv@gmail.com>
2019-04-25 00:47:05 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import ReactSlider from "react-slider";
+import "./App.css";
+
+export default () => {
+  const def = 50;
+  const [nr, setnr] = useState(def);
+
+  const move = data => setnr(data);
+
+  return (
+    <div style={{ padding: "200px" }}>
+      <h1>{nr}</h1>
+      <div style={{ maxWidth: "500px" }}>
+        <ReactSlider defaultValue={def} withBars onChange={move} />
+      </div>
+    </div>
+  );
+};
